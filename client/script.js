acc4645ddd099ca1e63dd0b9fde005c3c064e0ad
Ethan Tecson <ethantecson@gmail.com>
2024-10-01 23:22:20 -0400

    POST from frontend solved

--- client/script.js
+++ client/script.js
@@ -1,9 +1,21 @@
+const input = document.getElementById("classInput");
+const output = document.getElementById("output");
+const button = document.getElementById("inputButton")
+
 function fetchRecords() {
     fetch('/api/records')
         .then(response => response.json())
         .then(data => {
             console.log(data);
-        })
-}
+        });
+};
+
+function makeRecord() {
+    output.innerHTML = input.value;
+};
+
+
+
 
 document.getElementById("fetchRecordsButton").addEventListener('click', fetchRecords);
+button.addEventListener('click',makeRecord);
